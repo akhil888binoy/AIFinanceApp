99dbe5226e8f18170306982413c9fa4175bc091b
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-09-26 14:35:30 +0530

    theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,29 @@
-import { createTheme } from "@mui/material"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
+import { Box } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { themeSettings } from "./theme";
+import Navbar from "@/scenes/navbar";
+
 
 function App() {
-const theme = useMemo(()=> createTheme(themeSettings))
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <>
-      
-    </>
-  )
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+             
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
