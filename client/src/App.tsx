127872096a61bbe65065b52be5bf4a9eeedc9544
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-09-26 13:56:12 +0530

    theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,9 @@
+import { createTheme } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
 
 function App() {
-
+const theme = useMemo(()=> createTheme(themeSettings))
   return (
     <>
       
